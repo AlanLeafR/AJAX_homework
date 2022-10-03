51936a671312e504ceb86cb257a584bcb70fdb6f
AlanLeafR <ran315431@gmail.com>
2022-10-03 21:00:01 +0800

    1003

---
+++ AJAX_homework/AJAX_homework/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using AJAX_homework.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AJAX_homework.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //引用 js/travels，將資料讀出來顯示在網頁上
+        public IActionResult TaoyuanView()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
